4f66ae946bf4afef4748001699abd3bbf0feda13
arthur <arthur.rios007@gmail.com>
2025-04-16 13:33:07 -0300

    feat: add loader

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,6 +20,8 @@ export async function verifyJwt(): Promise<null | string> {
   }
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+
     const { sub: userId } = verify(accessToken, env.JWT_SECRET) as JwtPayload
 
     if (!userId) {
